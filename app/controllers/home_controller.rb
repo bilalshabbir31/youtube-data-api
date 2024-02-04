769eaeac3dfea8d2c6c4534e0646820153787181
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-02-04 16:27:46 +0500

    Add Home's controller and their index page.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def fetch_channel_videos
+    yt_channel_id = params[:yt_channel_id]
+    ChannelVideoFetchJob.perform_now(yt_channel_id)
+    flash[:notice] = "Fetching Channel Data: #{yt_channel_id}"
+    redirect_to root_path
+  end
+end
