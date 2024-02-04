ded43f1991bd9ba9274d7568dae3c103afc13f05
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-02-04 17:54:13 +0500

    Set job adapter to sidekiq

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module Youtube
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
