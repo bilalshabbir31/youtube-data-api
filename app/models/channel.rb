bee71c5597e3105ad7b05268f5db01ddf0bce723
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-02-06 21:50:55 +0500

    Annotate models

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: channels
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  yt_channel_id :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Channel < ApplicationRecord
   has_many :videos
 end
