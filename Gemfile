4235a2d6ad35407b45f955f36746319c58005aca
Muhammad Bilal <50591961+bilalshabbir31@users.noreply.github.com>
2024-02-07 10:09:57 +0500

    chore/Annotate-Models (#9)
    * Added Annotate gem
    
    * Annotate models

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem 'bootsnap', require: false
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+  gem 'annotate', '~> 3.2'
   gem 'debug', platforms: %i[mri windows]
 end
 
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: videos
+#
+#  id           :bigint           not null, primary key
+#  yt_video_id  :string
+#  title        :text
+#  description  :text
+#  duration     :string
+#  tags         :text
+#  published_at :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  channel_id   :bigint           not null
+#
 require 'test_helper'
 
 class VideoTest < ActiveSupport::TestCase
