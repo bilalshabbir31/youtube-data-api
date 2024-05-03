d1037ba5368c3cd65f219c3ecaf39c7bad684f52
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-05-03 12:24:24 +0500

    code refactor

--- app/workers/video_data_fetch_worker.rb
+++ app/workers/video_data_fetch_worker.rb
@@ -8,15 +8,21 @@ class VideoDataFetchWorker
   def perform(video_id, channel_id)
     ya_client = youtube_service_credentials
     response = ya_client.list_videos('snippet,contentDetails', id: video_id)
-    video_hash = {
-      yt_video_id: video_id,
-      title: response.items.first.snippet.title,
-      description: response.items.first.snippet.description,
-      duration: response.items.first.content_details.duration,
-      tags: response.items.first.snippet.tags,
-      published_at: response.items.first.snippet.published_at,
-      channel_id:
-    }
-    Video.find_or_create_by(yt_video_id: video_id).update(video_hash)
+    record = build_record(video_id, response, channel_id)
+    Video.find_or_create_by(yt_video_id: video_id).update(record)
   end
+
+  private
+
+    def build_record(video_id, response, channel_id)
+      {
+        yt_video_id: video_id,
+        title: response.items.first.snippet.title,
+        description: response.items.first.snippet.description,
+        duration: response.items.first.content_details.duration,
+        tags: response.items.first.snippet.tags,
+        published_at: response.items.first.snippet.published_at,
+        channel_id:
+      }
+    end
 end
