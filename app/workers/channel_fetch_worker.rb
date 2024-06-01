6ad23baeff0e12bd9b269f8da2da15151880d819
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-06-01 13:46:41 +0500

    add channel meta data fetch worker

---
+++ app/workers/channel_fetch_worker.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class ChannelFetchWorker
+  include Sidekiq::Worker
+  include YoutubeReportingCredentialsService
+  
+  def perform(yt_channel_id)
+    ya_client = youtube_service_credentials
+    response = ya_client.list_channels('snippet,contentDetails,statistics, status', id: yt_channel_id)
+    create_channel(response)
+  end
+
+  private
+
+    def create_channel(response)
+      items = response.items.first
+      statistics = items.statistics
+      channel = Channel.find_or_initialize_by(yt_channel_id: items.id)
+      channel.name = items.snippet.title
+      channel.description = items.snippet&.description
+      channel.custom_url = items.snippet.custom_url
+      channel.published_at = items.snippet.published_at
+      channel.hidden_subscriber_count = statistics.hidden_subscriber_count
+      channel.subscriber_count = statistics.subscriber_count
+      channel.video_count = statistics.video_count
+      channel.view_count = statistics.view_count
+      channel.save if channel.changed?
+    end
+end
