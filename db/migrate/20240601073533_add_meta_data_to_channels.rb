853931dc49fad1d77d12ffdc5e94cbbb35e65655
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-06-01 12:48:53 +0500

    rename attribute names

--- db/migrate/20240601073533_add_meta_data_to_channels.rb
+++ db/migrate/20240601073533_add_meta_data_to_channels.rb
@@ -5,9 +5,9 @@ class AddMetaDataToChannels < ActiveRecord::Migration[7.1]
     add_column :channels, :description, :string
     add_column :channels, :custom_url, :string
     add_column :channels, :published_at, :datetime
-    add_column :channels, :viewCount, :bigint
-    add_column :channels, :subscriberCount, :bigint
-    add_column :channels, :hiddenSubscriberCount, :boolean
-    add_column :channels, :videoCount, :bigint
+    add_column :channels, :view_count, :bigint
+    add_column :channels, :subscriber_count, :bigint
+    add_column :channels, :hidden_subscriber_count, :boolean
+    add_column :channels, :video_count, :bigint
   end
 end
