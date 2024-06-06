6e190a00f91cf04bc9b0575f3227aa5ec23cb83e
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-06-06 22:11:09 +0500

    fixed rubocop all offenses

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 class ChannelsController < ApplicationController
-  before_action :set_channel, only: %i[ show destroy ]
+  before_action :set_channel, only: %i[show destroy]
 
   def index
     @channels = Channel.all
   end
 
-  def show
-  end
+  def show; end
 
   def destroy
     @channel.destroy!
 
     respond_to do |format|
-      format.html { redirect_to channels_url, notice: "Channel was successfully destroyed." }
+      format.html { redirect_to channels_url, notice: 'Channel was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
+
     def set_channel
       @channel = Channel.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web, at: '/sidekiq'
   get 'up' => 'rails/health#show', as: :rails_health_check
-  
-  resources :channels, except: [:new, :edit, :create, :update]
-  
+
+  resources :channels, except: %i[new edit create update]
+
   resources :home, only: :index do
     collection do
       get 'fetch_channel_videos', to: 'home#fetch_channel_videos', as: 'fetch_channel_videos'
